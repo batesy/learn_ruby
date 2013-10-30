809a582be23f96e6e133ab1fd50930a91e8fff68
batesy <gbatesy@gmail.com>
2013-10-30 11:54:54 -0400

    Finished MakeChange exercise.

--- make_change/make_change.rb
+++ make_change/make_change.rb
@@ -1,40 +1,17 @@
-# class Changer
-#   def self.make_change(x)
-#     change = []
-#     if x % 25 == 0
-#       change.push(25) * (x/25)
-#     elsif x % 10 == 0
-#       change.push(10) * (x/10)
-#     end
-#   end
-# end
-
-# class Changer
-
-#   def self.make_change(x)
-#     change = []
-#     quarters_to_array
-#   end
-
-#   def self.quarters
-#     x/25
-#   end
-
-#   def self.quarters_to_array
-#     change << quarters
-#   end
-# end
-
 class Changer
   def self.make_change(x)
     change = []
-    quarters = x/25
+    quarters = x / 25
     x = x % 25
     dimes = x/10
     x = x % 10
     nickels = x/5
     x = x % 5
     pennies = x
-    change.push(25) * quarters
+    quarters.times { |x| change.push(25)}
+    dimes.times { |x| change.push(10)}
+    nickels.times { |x| change.push(5)}
+    pennies.times { |x| change.push(1)}
+    change
   end
 end
